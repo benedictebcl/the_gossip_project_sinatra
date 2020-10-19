fed13a7e2a9f9172bfe5e1f53238592e8067205f
benedictebcl <bouclonbenedicte@gmail.com>
2020-10-19 18:53:54 +0200

    add

---
+++ lib/controller.rb
@@ -0,0 +1,36 @@
+require 'bundler'
+Bundler.require
+
+require 'gossip'
+
+class ApplicationController < Sinatra::Base
+	get '/' do
+	  erb :index, locals: {gossips: Gossip.all} 
+	end
+
+	get '/gossips/new/' do
+	  erb :new_gossip
+	end
+
+	get '/gossips/:id/' do
+		erb :show
+	end
+
+	get '/gossips/:id/edit/' do
+		erb :edit
+	end 
+
+	post '/gossips/new/' do
+		Gossip.new(params["gossip_author"], params["gossip_content"]).save
+		redirect '/'
+	end
+
+	post '/gossips/edit/' do
+		Gossip
+	end
+
+
+
+
+
+end
